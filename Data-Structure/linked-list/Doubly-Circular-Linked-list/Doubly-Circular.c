e1052b9a24994a170baef80f4f98f2afe1f38326
Pratik Bhusari <pratikbhusari77@gmail.com>
2022-12-15 16:24:04 +0530

    Doubly Circular Linked List Code Added

--- Data-Structure/linked-list/Doubly-Circular-Linked-list/Doubly-Circular.c
+++ Data-Structure/linked-list/Doubly-Circular-Linked-list/Doubly-Circular.c
@@ -29,17 +29,15 @@ void addNode(){
 
     if(head==NULL){
         head=newNode;
+        head->next=head;
+        head->prev=head;
     }
     else {
 
-        nd * temp=head;
-
-        while(temp->next!=head){
-            temp=temp->next;
-        }
-        temp->next=newNode;
-        newNode->prev=temp;
+        head->prev->next=newNode;
+        newNode->prev=head->prev;
         newNode->next=head;
+        head->prev=newNode;
     }
 }
 
@@ -47,10 +45,11 @@ int count(){
 
 	nd * temp =head;
 	int cnt = 0;
-	while(temp!=NULL){
+	while(temp->next!=head){
 		cnt++;
 		temp=temp->next;
 	}
+    cnt++;
 	return cnt;
 }
 void addAtFirst(){
@@ -59,14 +58,17 @@ void addAtFirst(){
 
     if(head==NULL){
         head=newNode;
+        head->next=head;
+        head->prev=head;
     }
     else{
 
-        nd * temp=head;
-
-        newNode->next=head;
+        head->prev->next=newNode;
+        newNode->prev=head->prev;
         head->prev=newNode;
+        newNode->next=head;
         head=newNode;
+
     }
 }
 
@@ -78,6 +80,7 @@ void addAtLast(){
 int addAtPos(int pos){
 
     nd * newNode=createNode();
+    
     int cnt=count();
 
     if(head==NULL){
@@ -86,22 +89,189 @@ int addAtPos(int pos){
     else if(pos<1 || pos>cnt+1){
         printf("Invalid Position Entered !");
     }
+    else if(pos==1){
+        addAtFirst();
+    }
     else if(pos==cnt+1){
 
         addAtLast();
     }
     else {
 
+        
         nd * temp=head;
 
         while(pos-2){
 
             temp=temp->next;
             pos--;
+  
         }
-        new
+        newNode->next=temp->next;
+        newNode->next->prev=newNode;
+        newNode->prev=temp;
+        temp->next=newNode;
+    }
+}
+
+void deleteFirst(){
+
+    if(head==NULL){
+
+        printf("Your Linked List is empty ");
+    }
+    else {
+
+        nd * temp=head;
+        head->next->prev=head->prev;
+        head->prev->next=head->next;
+        head=head->next;
+        free(temp);
+    }
+}
+
+void deleteLast(){
+    if(head==NULL){
+        printf("Your Linked list is Empty");
+    }
+    else if(head->next==head){
+        deleteFirst();
+    }
+    else {
+
+        head->prev=head->prev->prev;
+        free(head->prev->next);
+        head->prev->next=head;
+    }
+}
+
+int deleteAtpos(int pos){
+
+    int cnt=count();
+
+    if(head==NULL){
+        printf("Your Linked List is Empty");
+    }
+    else if(pos<1 || pos>cnt){
+
+        printf("Invalid Position Entered ");
 
     }
+    else if(pos==cnt){
+            deleteLast();
+    }
+    else if(pos==1){
+        deleteFirst();
+    }
+    else {
 
-    
+        nd * temp=head;
+
+        while(pos-2){
+            temp=temp->next;
+        }
+        temp->next=temp->next->next;
+        free(temp->next->prev);
+        temp->next->prev=temp;
+    }
+}
+void printSCLL(){
+	
+	nd * temp=head;
+
+	if(head==NULL){
+		printf("Your Linked List is Empty !");
+	}
+	else {
+		while(temp->next!=head){
+			printf("|%d|->",temp->data);
+			temp=temp->next;
+		}
+		printf("|%d|",temp->data);
+	}
+
+}
+
+void checkDoubly(){
+
+    nd * temp=head;
+
+    while(temp!=NULL){
+        printf("%d",temp->data);
+        temp=temp->next;
+    }
+}
+void main(){
+
+	char ch;
+	int pos,pos1,num,nd;
+
+	do{
+		printf("Which Operation do You want : \n");
+		printf("1.addNode\n");
+		printf("2.Count Nodes in Linked List\n");
+		printf("3.Add at First\n");
+		printf("4.Add at Position\n");
+		printf("5.Add at last \n");
+		printf("6.Delete first\n");
+		printf("7.Delete at Position\n");
+		printf("8.Delete last\n");
+		printf("9.Print Linked List\n");
+
+		int choice;
+		printf("Enter Your Choice : \n");
+		scanf("%d",&choice);
+
+		switch(choice){
+
+			case 1 : 
+				printf("How Many Nodes Do You want to create : ");
+				scanf("%d",&nd);
+				for(int i=0 ; i<nd ; i++){
+					addNode();
+				}
+				break;
+			case 2 : 
+				printf("Count of Nodes in Linked List Are : %d",count());
+				break;
+			case 3 : 
+				addAtFirst();
+				break;
+			case 4 : 
+				printf("At which Position do You want to insert : \n");
+				scanf("%d",&pos);
+				addAtPos(pos);
+				break;
+			case 5 :
+				addAtLast();
+				break;
+			case 6 : 
+				deleteFirst();
+				break;
+			case 7 : 
+				printf("At which position do you want to delete : \n");
+				scanf("%d",&pos1);
+				deleteAtpos(pos1);
+				break;
+			case 8 :
+				deleteLast();
+				break;
+			case 9 :
+				printSCLL();
+				break;
+            case 10 :
+                checkDoubly();
+                break;
+			default :
+				printf("Invalid Input : ");
+		
+		
+		}
+		getchar();
+		printf("\nDo you want any other Functionalities again if yes type 'Y' || 'y' : ");
+		scanf("%c",&ch);
+	}
+	while(ch=='y'||ch=='Y');
+
+
 }
